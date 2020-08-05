f93efbe71f02aa6b675474ddd81cc57756630f49
Lulas <luisgpereda@gmail.com>
2020-08-05 23:15:32 +0200

    subtle changes

--- src/StabilityKolmogorov.jl
+++ src/StabilityKolmogorov.jl
@@ -1,13 +1,13 @@
 module StabilityKolmogorov
 
     #Dependencies on other packages
-    import Flows
-    import OpenKolmogorovFlow
-    import DelimitedFiles
-    import Statistics
-    import Glob
-    import Serialization
-    import Random
+    using Flows
+    using OpenKolmogorovFlow
+    using DelimitedFiles
+    using Statistics
+    using Glob
+    using Serialization
+    using Random
 
     include("setup.jl")
     include("Perturbation.jl")
