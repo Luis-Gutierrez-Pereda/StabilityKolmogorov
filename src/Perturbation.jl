8ee6b944a373ce16a79fb6303b789e7e14610d51
Lulas <luisgpereda@gmail.com>
2020-08-05 23:57:42 +0200

    try

--- src/Perturbation.jl
+++ src/Perturbation.jl
@@ -26,7 +26,7 @@ function Perturbation(SetupChoice::Int,
     # forward map
     ϕ = flow(N, L, CB3R2R3e(Ω), stepping);
     
-  mon = Monitor(ω);
+    
 
     #initial 50 time units to settle the turbulent flow
     ϕ(Ω, (0, 50), reset!(mon));
